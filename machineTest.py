20164f10cd9c29cdfb2f4c526440e16890326c81
Jwallee <grantrobinett@gmail.com>
2023-06-27 16:39:32 -0500

    Updated Analysis

--- machineTest.py
+++ machineTest.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 
 specs = ["Non","Fatal"]
-# classifiers = ["N - NOT INJURED","B - MINOR INJURY", "K - FATAL INJURY"]
 classifiers = ["N - NOT INJURED", "K - FATAL INJURY"]
 speeds = []
+weathers = []
+lights = []
 
 # LITERALLY JUST READING INFO
 # Here, we read the files present in the folder path specified (crashes)
@@ -18,17 +19,37 @@ def get_file_names(folder_path):
     return file_names
 
 # Read the Excel spreadsheet
-df = pd.read_excel('crashes/NonDec2022Update.xlsx', usecols='B', skiprows=2)
-raw = df.values
-# Print the speeds array
-for a in raw:
+sdf = pd.read_excel('crashes/NonDec2022Update.xlsx', usecols='B', skiprows=2)
+wdf = pd.read_excel('crashes/NonDec2022Update.xlsx', usecols='D', skiprows=2)
+ldf = pd.read_excel('crashes/NonDec2022Update.xlsx', usecols='C', skiprows=2)
+rawS = sdf.values
+for a in rawS:
     for l in a:
         speeds.append(l)
-df = pd.read_excel('crashes/Fatal2022Update.xlsx', usecols='B', skiprows=2)
-raw = df.values
-for a in raw:
+rawW = wdf.values
+for a in rawW:
+    for l in a:
+        weathers.append(l)
+rawL = ldf.values
+for a in rawL:
+    for l in a:
+        lights.append(l)
+
+sdf = pd.read_excel('crashes/Fatal2022Update.xlsx', usecols='B', skiprows=2)
+wdf = pd.read_excel('crashes/Fatal2022Update.xlsx', usecols='D', skiprows=2)
+ldf = pd.read_excel('crashes/Fatal2022Update.xlsx', usecols='C', skiprows=2)
+rawS = sdf.values
+for a in rawS:
     for l in a:
         speeds.append(l)
+rawW = wdf.values
+for a in rawW:
+    for l in a:
+        weathers.append(l)
+rawL = ldf.values
+for a in rawL:
+    for l in a:
+        lights.append(l)
 # print(speeds)
 
 
@@ -69,7 +90,7 @@ for run in range(0,groups):
 
 
 # # ACTUAL DATA PROCESSING STUFF
-def testing(narratives,classifications,speed):
+def testing(narratives,classifications,speed,light):
     from sklearn.feature_extraction.text import CountVectorizer
     from sklearn.model_selection import train_test_split, GridSearchCV
     from sklearn.preprocessing import LabelEncoder
@@ -89,32 +110,50 @@ def testing(narratives,classifications,speed):
 
     label_encoder = LabelEncoder()
     speeds_encoded = label_encoder.fit_transform(speed)
+    # weather_encoded = label_encoder.fit_transform(weather)
+    light_encoded = label_encoder.fit_transform(light)
 
-    X_combined = sp.hstack([X_narratives, sp.csr_matrix(speeds_encoded).T])
+    X_combined = sp.hstack([X_narratives, sp.csr_matrix(speeds_encoded). T, sp.csr_matrix(light_encoded).T])
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X_combined, classifications, test_size=0.2, random_state=42)
+    
+    
     param_grid = {
-    'n_estimators': [50, 100, 150, 200, 250],
-    'max_depth': [None, 5, 10, 15, 20]
+    'n_estimators': [200, 300, 500, 600, 800],
+    'max_depth': [25, 50, 75, 100, 125]
     }
 
 
 
     # Instantiate and train the logistic regression classifier
     classifier = RandomForestClassifier()
-    grid_search = GridSearchCV(classifier, param_grid, cv=5)
+    # Checking efficienceies of each parameter combinaion
+
+    grid_search = GridSearchCV(classifier, param_grid, cv=8)
     grid_search.fit(X_train, y_train)
+    results = grid_search.cv_results_
+    params = results['params']
+    mean_scores = results['mean_test_score']
+    # Print the accuracies for each parameter combination
+    for param, score in zip(params, mean_scores):
+        print("Parameters:", param)
+        print("Mean Accuracy:", score)
+        print()
+
+
 
     # classifier.fit(X_train, y_train)
 
     # Make predictions on the testing set
     # test_case = ["Passenger in unit 1 had a no injuries"]
     # test_case_transformed = vectorizer.transform(test_case)
-    best_params = grid_search.best_params_
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
+
+    # best_params = grid_search.best_params_
+    # best_model = grid_search.best_estimator_
+    # y_pred = best_model.predict(X_test)
     # y_pred = classifier.predict(X_test)
+
     # y_pred = classifier.predict(test_case_transformed)
     
 
@@ -123,11 +162,11 @@ def testing(narratives,classifications,speed):
     # print("Accuracy:", accuracy)
 
     # Perform cross-validation
-    cv_scores = cross_val_score(classifier, X_combined, classifications, cv=5)
+    # cv_scores = cross_val_score(classifier, X_combined, classifications, cv=5)
 
-    # Print the cross-validation scores
-    print("Cross-Validation Scores:", cv_scores)
-    print("Mean Accuracy:", cv_scores.mean())
+    # # Print the cross-validation scores
+    # print("Cross-Validation Scores:", cv_scores)
+    # print("Mean Accuracy:", cv_scores.mean())
 
 
 
@@ -139,4 +178,5 @@ def testing(narratives,classifications,speed):
 # print(classify)
 # print(total)
 
-testing(total,classify,speeds)
+# testing(total,classify,speeds,weathers,lights)
+testing(total,classify,speeds,lights)
